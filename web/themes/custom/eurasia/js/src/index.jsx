c56b058634b3f0944c0f953a050123af9b7f15ea
Johann Schwabl <schwabl@infill.com>
2023-01-01 16:43:29 +0700

    After install react, create react basic block

---
+++ web/themes/custom/eurasia/js/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// # Example 1: Simple "Hello, World" code
+ReactDOM.render(
+  <h1><h2>I Love YOU</h2><h3>Sok Poeurn!</h3> </h1>,
+  document.getElementById('react-app')
+);
